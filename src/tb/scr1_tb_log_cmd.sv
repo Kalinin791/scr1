cd7cbb45cbfc1a9d9ee4f0a8c6fc6deb0f4cb1f0
Ivan Kalinin <doom.kalinin2015@yandex.ru>
2026-05-21 01:29:44 +0300

    Final: delayed register output for xor detection

--- src/tb/scr1_tb_log_cmd.sv
+++ src/tb/scr1_tb_log_cmd.sv
@@ -7,13 +7,21 @@ module scr1_tb_log_cmd(
 wire [31:0] rs1_data;
 wire [31:0] rs2_data;
 
+// Задержка на 1 такт
+logic detected_xor;
+
 assign rs1_data = scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.mprf2exu_rs1_data;
 assign rs2_data = scr1_top_tb_ahb.i_top.i_core_top.i_pipe_top.mprf2exu_rs2_data;
 
 always_ff @(posedge clk) begin
-    if ((imem_rdata[6:0] == 7'b0110011) &&
-        (imem_rdata[14:12] == 3'b100) &&
-        (imem_rdata[31:25] == 7'b0000000)) begin
+    // Детектируем xor
+    detected_xor <= (imem_rdata[6:0] == 7'b0110011) &&
+                    (imem_rdata[14:12] == 3'b100) &&
+                    (imem_rdata[31:25] == 7'b0000000);
+end
+
+always_ff @(posedge clk) begin
+    if (detected_xor) begin
         $display("DETECTED: xor instruction");
         $display("  rs1_data = 0x%0h", rs1_data);
         $display("  rs2_data = 0x%0h", rs2_data);
